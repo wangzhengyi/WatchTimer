f9e9c6b34cd9fe1012e5ff658a8efc0bb5e980ed
wzy <zhengyi.wzy@alibaba-inc.com>
2016-04-11 16:01:51 +0800

    delete useless import

--- src/main/java/com/watch/timer/view/TimerWatchView.java
+++ src/main/java/com/watch/timer/view/TimerWatchView.java
@@ -9,7 +9,6 @@ import android.graphics.PointF;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
